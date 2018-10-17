15491795aa2fea8f2b31d3bd4c800cddeebd9f54
kane.chong <kane.chong@lytx.com>
2018-10-16 17:35:02 -0700

    Properly whitelist domain for CORS based on environment."

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -2,10 +2,11 @@
 
 module.exports = (req, res, next) => {
 
-    const origins = ['http://localhost:3000', 'http://localhost:4201', 'https://kane-pwa.firebaseapp.com'];
+    const origin = process.env.NODE_ENV === 'production'
+        ? 'https://kane-pwa.firebaseapp.com'
+        : 'http://localhost:4201';
 
-    origins.forEach((origin) => res.setHeader('Access-Control-Allow-Origin', origin));
-
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     next();
